8a214cc4cb715826adc953575ffd4395658ee84f
Yang Bai <cedriccan48@gmail.com>
2018-04-28 21:46:13 +0800

    changed js

--- js/script.js
+++ js/script.js
@@ -42,9 +42,9 @@ $(".button").on('click', function(){
 $(".box").hover(function(){
 	    if(!($(this).hasClass('box-filled-1') || $(this).hasClass('box-filled-2'))){
 		    if(playO_turn)
-				$(this).css("background-image", "url(../tic-tac-toe-v3/img/o.svg)");
+				$(this).css("background-image", "url(img/o.svg)");
 			else
-				$(this).css("background-image", "url(../tic-tac-toe-v3/img/x.svg)");
+				$(this).css("background-image", "url(img/x.svg)");
 			}
 	    }, function(){
 	    if(!($(this).hasClass('box-filled-1') || $(this).hasClass('box-filled-2')))
@@ -59,7 +59,7 @@ $(".box").click(function(){
 	    $('#player1').removeClass('active');
 	    $('#player2').addClass('active');
 		$(this).addClass('box-filled-1');
-		$(this).css("background-image", "url(../tic-tac-toe-v3/img/o.svg)");
+		$(this).css("background-image", "url(img/o.svg)");
 		playO_turn = false;
 	}
 	else
@@ -67,7 +67,7 @@ $(".box").click(function(){
 		$('#player2').removeClass('active');
 	    $('#player1').addClass('active');
 		$(this).addClass('box-filled-2');
-		$(this).css("background-image", "url(../tic-tac-toe-v3/img/x.svg)");
+		$(this).css("background-image", "url(img/x.svg)");
 		playO_turn = true;
 	}
 	win_state();
